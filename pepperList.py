ec6768b2a7042e5320debda477d1b6974f6bc6c4
cezaryszpotek <cezary.szpotek@gmail.com>
2022-12-01 16:56:18 +0100

    Add argparse

--- pepperList.py
+++ pepperList.py
@@ -7,6 +7,7 @@ from selenium import common
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
+import argparse
 import time
 import shelve
 import sys
@@ -17,36 +18,30 @@ import datetime
 
 
 def seleniumOptions(headless_param):
-
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_argument('--enable-javascript')
 
-    if headless_param:
+    if headless_param == 'True':
         options.add_argument('--headless')
 
-    #return webdriver.Chrome(service=Service("./chromedriver"), options=options)
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    
 
 
-def siteClickDelay(driver, xpth):   #exception catcher when clicking
-    break_flag=False
+def siteClickDelay(driver, xpth):  # exception catcher when clicking
+    break_flag = False
     try:
         wait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpth))).click()
     except common.exceptions.StaleElementReferenceException as StaleExcept:
-        #print('StaleExcept')
         pass
         try:
             driver.find_element(By.XPATH, xpth).click()
         except common.exceptions.NoSuchElementException:
-            #print('Nosuchelem')
-            break_flag=True
+            break_flag = True
             pass
     except common.exceptions.TimeoutException:
-        #print('Timeout')
         pass
     except common.exceptions.ElementClickInterceptedException:
         pass
@@ -54,11 +49,11 @@ def siteClickDelay(driver, xpth):   #exception catcher when clicking
     return break_flag
 
 
-def findUrl(shoplist, headless_param):  #finding url in foler/site
+def findUrl(shoplist, headless_param):  # finding url in foler/site
     db = shelve.open('url_list')
 
-    shopdict=dict()
-    notFound=[]
+    shopdict = dict()
+    notFound = []
 
     for shopname in shoplist:
         if shopname in db:
@@ -68,15 +63,15 @@ def findUrl(shoplist, headless_param):  #finding url in foler/site
             print('Searching for %s link' % shopname)
             notFound.append(shopname)
 
-
-    if len(notFound)!=0:
-        driver=seleniumOptions(headless_param)
+    if len(notFound) != 0:
+        driver = seleniumOptions(headless_param)
         driver.get('https://pepper.pl')
-        siteClickDelay(driver , '/html/body/div[1]/div[2]/div/section/div/div/div/div/div/div[2]/div[2]/button[1]')  # allow cookies
+        siteClickDelay(driver,
+                       '/html/body/div[1]/div[2]/div/section/div/div/div/div/div/div[2]/div[2]/button[1]')  # allow cookies
 
         for unknownUrl in notFound:
             link = urlScrap(driver, unknownUrl)
-            if link!=None:
+            if link != None:
                 shopdict[unknownUrl] = link
                 db[unknownUrl] = link
                 print(link)
@@ -86,7 +81,8 @@ def findUrl(shoplist, headless_param):  #finding url in foler/site
 
     return shopdict
 
-def urlScrap(driver, shopname):     #scrapping url from site
+
+def urlScrap(driver, shopname):  # scrapping url from site
     textbox = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/header/div/div/div[2]/form/div/span/input')
     textbox.send_keys(Keys.CONTROL + 'a')
     textbox.send_keys(Keys.DELETE)
@@ -107,13 +103,13 @@ def urlScrap(driver, shopname):     #scrapping url from site
     if shopname in namedict:
         return namedict[shopname]
     else:
-        if namedict!={}:
-            print(shopname,'not found. Are you looking for:')
+        if namedict != {}:
+            print(shopname, 'not found. Are you looking for:')
             for key in namedict:
                 print('-%s' % key)
             ipt = input()
 
-            try :
+            try:
                 return namedict[ipt]
             except KeyError:
                 print('Please specify shop name')
@@ -121,46 +117,28 @@ def urlScrap(driver, shopname):     #scrapping url from site
             print(shopname, ' not found')
 
 
-def searchingCore(name, url, headless_param, parameters):   #products search
-    temp = 500
-    pages_num = 5
-    days = 7
+def searchingCore(name, url, parameters):  # products search
 
-    for arg in parameters:
-        if '=' in arg:
-            if '--temp' in arg or '-t' in arg:
-                temp = int(arg[arg.find('=') + 1:])
-            elif '--days' in arg or '-d' in arg:
-                days = int(arg[arg.find('=') + 1:])
-            elif '--pages_num' in arg or '-p' in arg:
-                pages_num = int(arg[arg.find('=') + 1:])
-            else:
-                print(arg, ' - parameter not found!')
-        else:
-            print("Given parameter doesn`t have equal sign or it doesn`t exists!")
+    driver = seleniumOptions(parameters.visible)
 
-    if days is not None:
-        print('\n%s products list from %s days ago:' % (name.title(), days))
-    else:
-        print('\n%s products list:' % name.title())
-
-    driver = seleniumOptions(headless_param)
+    print('\n%s products list from %s days ago:' % (name.title(), parameters.days_ago))
 
     try:
         driver.get(url)
     except:
         print('Cannot get access to ', url)
 
-    siteClickDelay(driver, '/html/body/div[1]/div[2]/div/section/div/div/div/div/div/div[2]/div[2]/button[1]/span')  # allow cookies else pass
+    siteClickDelay(driver,
+                   '/html/body/div[1]/div[2]/div/section/div/div/div/div/div/div[2]/div[2]/button[1]/span')  # allow cookies else pass
     siteClickDelay(driver, '//*[@id="tour-expired"]/span[2]/span[2]')  # filter bar click
     siteClickDelay(driver, '/html/body/section/div/ul/li[1]/form/ul/li[1]/label/span/span')  # outdated filter
 
-    for i in range(pages_num):  # rewind more pages
+    for i in range(parameters.pages_num):  # rewind more pages
         flag = siteClickDelay(driver, '//*[@id="merchant-deals"]/button')
         if flag:
             break
 
-    time.sleep(1) #waiting to load all items
+    time.sleep(1)  # waiting to load all items
 
     try:  # try to get items
         items = driver.find_elements(By.CLASS_NAME, "threadGrid.thread-clickRoot")
@@ -169,7 +147,7 @@ def searchingCore(name, url, headless_param, parameters):   #products search
 
         for item in items:  # iterate over items
             name = item.find_element(By.CLASS_NAME, 'cept-tt').text  # product name
-            if days != None:
+            if parameters.days_ago != None:
                 date = item.find_element(By.CLASS_NAME, 'hide--toW3').text  # product add-date
 
             try:
@@ -184,9 +162,7 @@ def searchingCore(name, url, headless_param, parameters):   #products search
             except common.exceptions.NoSuchElementException:
                 price = 'unknown'
 
-            if points_given > int(temp):
-                #if days != None:
-                    #print(date, dateCheck(date, days), '\n')
+            if points_given > int(parameters.temp):
                 item_dict[points_given] = name + ': ' + price  # add to list
     except:
         print('Cannot get access to items')
@@ -198,10 +174,10 @@ def searchingCore(name, url, headless_param, parameters):   #products search
         txt += str(key) + ": " + str(item_dict[key]) + '\n'
         txt += '\n'
 
-
     return txt
 
-def dateCheck(givendate, days_ago):  #hardcoded product`s date check
+
+def dateCheck(givendate, days_ago):  # hardcoded product`s date check
     givendate = givendate.lower()
 
     if givendate.startswith('dzisiaj') or givendate.startswith('obowiązuje do') or givendate[0].isdigit():
@@ -211,7 +187,7 @@ def dateCheck(givendate, days_ago):  #hardcoded product`s date check
     else:
         day = int(''.join([x for x in givendate if x.isdigit()]))
         month = ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru'].index(
-            ''.join([x for x in givendate if x.isalpha()]).strip())+1
+            ''.join([x for x in givendate if x.isalpha()]).strip()) + 1
 
         tdy = int(datetime.datetime.today().day)
         currentmonth = int(datetime.datetime.today().month)
@@ -220,7 +196,8 @@ def dateCheck(givendate, days_ago):  #hardcoded product`s date check
         else:
             return True
 
-def makeASCIQr(data):   #making qr code
+
+def makeASCIQr(data):  # making qr code
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -228,7 +205,7 @@ def makeASCIQr(data):   #making qr code
         border=4,
     )
 
-    if data!='':
+    if data != '':
         qr.add_data(data)
         f = io.StringIO()
         qr.print_ascii(out=f)
@@ -238,31 +215,45 @@ def makeASCIQr(data):   #making qr code
     else:
         print('Items not found!')
 
-def argPass(args):
 
-    shoplist = []
-    parameterlist = []
-    headless_param = False
+def argPass(shoplist):
 
-    for arg in args:
-        if arg.startswith('-') or arg.startswith('--'):
-            if arg.strip('-') == 'h' or arg.strip('-') == 'headless':
-                headless_param = True
-            else:
-                parameterlist.append(arg)
-        else:
-            shoplist.append(arg.lower())
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Pepper list generator. Work only in python3',
+        usage='''pepperList.py shop_names [-h] [-v] [-t TEMP] [-d DAYS_AGO] [-p PAGES_NUM]''')
+    parser.add_argument(
+        '-v', '--visible', action='store_true',
+        default='True', help='disable browser headless mode')
+    parser.add_argument(
+        '-t', '--temp', type=int,
+        default=500, help='temperature of products on list')
+    parser.add_argument(
+        '-d', '--days_ago', type=int,
+        default=7, help='products search from X days ago')
+    parser.add_argument(
+        '-p', '--pages_num', type=int,
+        default=5, help='number of pages to search product')
 
-    shopdict = findUrl(shoplist, headless_param)
+    parameters = parser.parse_args()
+
+    shopdict = findUrl(shoplist, parameters.visible)
 
     for name in shopdict:
-        makeASCIQr(searchingCore(name, shopdict[name], headless_param, parameterlist))
+        makeASCIQr(searchingCore(name, shopdict[name], parameters))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        argPass(sys.argv[1:])
+        shoplist = []
+
+        for arg in sys.argv[1:]:
+            if arg.startswith('-') or arg.startswith('--'):
+                break
+            else:
+                shoplist.append(arg.lower())
+                sys.argv.remove(arg)
+
+        argPass(shoplist)
     else:
         print("Program available only in script mode.")
-
-
